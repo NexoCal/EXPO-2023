d2038518f4ab211e889dfff9b9514dfc06ffd232
TheMatery <raditmasterv@gmail.com>
2023-11-23 11:18:23 +0700

    table ga guna

--- database/landing.sql
+++ database/landing.sql
@@ -3,20 +3,20 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3308
--- Generation Time: Nov 20, 2023 at 01:17 PM
+-- Generation Time: Nov 23, 2023 at 05:16 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.0.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
--- PHP Version: 8.0.28
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
--- Server version: 10.4.28-MariaDB
+
 --
 -- Database: `landing`
 --
@@ -120,6 +120,18 @@ CREATE TABLE `gambar_things` (
   `id_gambar_things` int(11) NOT NULL,
   `gambar` varchar(255) NOT NULL,
   `id_things` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hapus saja`
+--
+
+CREATE TABLE `hapus saja` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `isi` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -242,6 +254,18 @@ CREATE TABLE `things` (
   `id_kategori_things` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `trip`
+--
+
+CREATE TABLE `trip` (
+  `id_trip` int(11) NOT NULL,
+  `judul` varchar(255) NOT NULL,
+  `isi` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 --
 -- Indexes for dumped tables
 --
@@ -311,6 +335,12 @@ ALTER TABLE `things`
   ADD KEY `id_kategori_things` (`id_kategori_things`);
 
 --
+-- Indexes for table `trip`
+--
+ALTER TABLE `trip`
+  ADD PRIMARY KEY (`id_trip`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -367,6 +397,12 @@ ALTER TABLE `story`
 --
 ALTER TABLE `things`
   MODIFY `id_things` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `trip`
+--
+ALTER TABLE `trip`
+  MODIFY `id_trip` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
