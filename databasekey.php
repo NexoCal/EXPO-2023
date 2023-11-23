4a9380214852533df3141f9934e07c77960a72a9
NexoCal <22523018@students.uii.ac.id>
2023-11-23 16:51:55 +0000

    fixing

--- databasekey.php
+++ databasekey.php
@@ -1,11 +1,10 @@
 <?php
-$user = 'root';
-$pass = '';
-$dbname = 'visityogyakarta';
+$user = 'NXCL';
+$pass = 'task-404';
+$dbname = 'visityogya';
 $servername = 'localhost';
 
 $conn = mysqli_connect($servername, $user, $pass, $dbname);
-
 
 if (!$conn) {
     die("koneksi gagal: " . mysqli_connect_error());
