cb9bdb3f3f7e4d6bbf66c2fe6625bafd15ad27ca
NexoCal <22523018@students.uii.ac.id>
2023-11-24 01:18:39 +0000

    testing git again

--- databasekey.php
+++ databasekey.php
@@ -3,6 +3,7 @@ $user = 'NXCL';
 $pass = 'task-404';
 $dbname = 'visityogya';
 $servername = 'localhost';
+
 $conn = mysqli_connect($servername, $user, $pass, $dbname);
 if (!$conn) {
     die("koneksi gagal: " . mysqli_connect_error());
